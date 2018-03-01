952c14057c6e026756d093d03f2b21cbcf7d0f6b
telpuhova <telpuhova@gmail.com>
2018-03-01 10:01:38 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+  start: boolean = false;
+  squares;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startGame() {
+    this.start = true;
+
+  }
+
+  clickSquare(clickedSquare) {
+
+  }
+
+}
