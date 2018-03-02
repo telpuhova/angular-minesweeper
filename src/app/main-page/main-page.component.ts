8b995659ca039d544bb7e24b986eba4b712950a2
Guest User <Guest@Epicodus-3A.local>
2018-03-01 16:04:32 -0800

    add subscribtion to observables

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -11,25 +11,34 @@ import { SquareService } from '../square.service';
 })
 export class MainPageComponent implements OnInit {
   start: boolean = true;
-  squares: FirebaseListObservable<any[]>;
-  // rows = this.splitIntoRows();
+  squares;
+  rows;
 
 
   constructor(private squareService: SquareService) { }
 
   ngOnInit() {
-    this.squares = this.squareService.getSquares();
-    console.log(this.squares);
+    // this.squares = this.squareService.getSquares();
+    // console.log(this.squares);
+
+
+    this.squareService.getSquares().subscribe(dataLastEmittedFromObserver => {
+      this.squares = dataLastEmittedFromObserver;
+      console.log(this.squares)
+      this.rows = this.splitIntoRows();
+    })
+
   }
 
-  // splitIntoRows() {
-  //   let rows=[];
-  //   let rowLength = Math.pow(this.squares.length, 0.5);
-  //   for (let i=0; i<this.squares.length; i+=rowLength) {
-  //     rows.push(this.squares.slice(i, i+rowLength));
-  //   }
-  //   return rows;
-  // }
+  splitIntoRows() {
+    let rows=[];
+    console.log("splitIntoRows: " + this.squares);
+    let rowLength = Math.pow(this.squares.length, 0.5);
+    for (let i=0; i<this.squares.length; i+=rowLength) {
+      rows.push(this.squares.slice(i, i+rowLength));
+    }
+    return rows;
+  }
 
   startGame() {
     this.start = true;
